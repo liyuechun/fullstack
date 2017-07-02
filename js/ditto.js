07cbb49b9daedc6b64cc53d641c846d23829bf1d
liyuechun <liyuechun2009@163.com>
2017-07-02 14:57:04 +0800

    update

--- js/ditto.js
+++ js/ditto.js
@@ -335,22 +335,22 @@ function router() {
     });
 
     // 加载disqus
-    (function() {
-      // http://docs.disqus.com/help/2/
-      window.disqus_shortname = 'fullstack';
-      window.disqus_identifier = (location.hash ? location.hash.replace("#", "") : 'READEME');
-      window.disqus_title = $(ditto.content_id + " h1").text();
-      window.disqus_url = 'http://fullstack.kongyixueyuan.com/' + (location.hash ? location.hash.replace("#", "") : 'README');
+    // (function() {
+    //   // http://docs.disqus.com/help/2/
+    //   window.disqus_shortname = 'fullstack';
+    //   window.disqus_identifier = (location.hash ? location.hash.replace("#", "") : 'READEME');
+    //   window.disqus_title = $(ditto.content_id + " h1").text();
+    //   window.disqus_url = 'http://fullstack.kongyixueyuan.com/' + (location.hash ? location.hash.replace("#", "") : 'README');
 
-      // http://docs.disqus.com/developers/universal/
-      (function() {
-        var dsq = document.createElement('script');
-        dsq.type = 'text/javascript';
-        dsq.async = true;
-        dsq.src = 'http://' + window.disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
-    })();
+    //   // http://docs.disqus.com/developers/universal/
+    //   (function() {
+    //     var dsq = document.createElement('script');
+    //     dsq.type = 'text/javascript';
+    //     dsq.async = true;
+    //     dsq.src = 'http://' + window.disqus_shortname + '.disqus.com/embed.js';
+    //     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    //   })();
+    // })();
 
     var perc = ditto.save_progress ? store.get('page-progress') || 0 : 0;
 
