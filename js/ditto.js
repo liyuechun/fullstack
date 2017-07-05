33383d9c3ffafe09ce472a23483e7e43a8e0e548
liyuechun <liyuechun2009@163.com>
2017-07-05 09:20:56 +0800

    update

--- js/ditto.js
+++ js/ditto.js
@@ -42,7 +42,7 @@ var getHash = function (hash) {
   }
 };
 
-var disqusCode = '<h3 style="font-size:25px">联系我，立即申请加入从零到壹全栈部落</h3><div id="disqus_thread"><ol><li>小精灵QQ：723591353</li><li><img src="http://orhm8wuhd.bkt.clouddn.com/xiaojingling.jpeg" /></li></ol></div>';
+var disqusCode = '<h3 style="font-size:25px">微信添加小精灵，立即申请加入从零到壹全栈部落</h3><div id="disqus_thread"><img src="http://orhm8wuhd.bkt.clouddn.com/xiaojingling.jpeg" /></div>';
 
 
 var menu = new Array();
